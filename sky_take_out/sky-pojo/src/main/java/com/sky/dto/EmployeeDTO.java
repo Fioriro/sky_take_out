3426f0c60218050e0a0ecbf7c6d7f9838d923ff7
Fioriro <fioriro@foxmail.com>
2024-02-15 22:54:14 +0800

    新增员工功能开发

--- sky_take_out/sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky_take_out/sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -4,6 +4,12 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+/**
+ * 员工数据传输对象
+ * 因为entity包含所有属性，而这些属性在传输中不需要
+ * 因此把entity简化为一个DTO，这样可以只传输需要的属性
+ * 从而提高系统性能
+ */
 @Data
 public class EmployeeDTO implements Serializable {
 
